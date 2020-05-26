36061f75d44501947281bec989d831b371f19fa9
laxatzi <laxatzi@yahoo.com>
2020-05-26 16:45:27 +0300

    work on for loops

--- domManipulation.js
+++ domManipulation.js
@@ -1,1 +1,31 @@
+(function () {
 
+   'use strict';
+// for...in loop
+  // example
+         let lunch = {
+            sandwich: 'ham',
+            snack: 'chips',
+            drink: 'soda',
+            desert: 'cookie',
+            guests: 3,
+            alcohol: false,
+      };
+            for(let prop in lunch){
+               if(lunch.hasOwnProperty(prop)){
+                  console.log('Property: '+ prop);
+                  console.log('Value: '+ lunch[prop]);
+            }
+         }// end loop
+// Array.forEach()  method
+ // example
+  //? *forEach functions cannot be terminated before completed.
+
+   let pets = ['dog', 'cat', 'donkey', 'canary'];
+   pets.forEach(function(pet, index) {
+      if(pet === 'cat') return;
+      console.log(index);
+      console.log(pet);
+   });
+
+})();
