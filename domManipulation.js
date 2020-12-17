55b5b41d1ac83a7b7ac03aefe4c94cc434b460b6
laxatzi <laxatzi@yahoo.com>
2020-12-17 11:06:03 +0200

    ADD: append node to parentNode

--- domManipulation.js
+++ domManipulation.js
@@ -1,45 +1,7 @@
-(function () {
-'use strict';
+"use strict";
 
-// for...in loop
-  // example
-         let lunch = {
-            sandwich: 'ham',
-            snack: 'chips',
-            drink: 'soda',
-            desert: 'cookie',
-            guests: 3,
-            alcohol: false,
-      };
-            for(let prop in lunch){
-               if(lunch.hasOwnProperty(prop)){
-                  console.log('Property: '+ prop);
-                  console.log('Value: '+ lunch[prop]);
-            }
-         }// end loop
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+ul.append(li);
 
-// Array.forEach()  method
- // example
-  //? *forEach functions cannot be terminated before completed.
-
-   let pets = ['dog', 'cat', 'donkey', 'canary'];
-   pets.forEach(function(pet, index) {
-      if(pet === 'cat') return;
-      console.log(index);
-      console.log(pet);
-   });
-
-// Computed Styles
-  // example
-  let element = document.querySelector('.firstClass');
-  let bgColor = window.getComputedStyle(element).backgroundColor;
-  console.log(bgColor);
-
-})();
-
-// accordion IIFE
-(function () {
-   'use strict';
-   
-
-})();
+console.log(ul.childNodes); // NodeList [ li ]
