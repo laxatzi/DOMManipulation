e62bcaaeea8c0def731aeb8da1c4b5b54b4788c8
laxatzi <laxatzi@yahoo.com>
2020-12-17 16:20:21 +0200

    Add: and remove elements by using DOM mannipulation methods

--- domManipulation.js
+++ domManipulation.js
@@ -1,7 +1,28 @@
 "use strict";
 
-const ul = document.createElement("ul");
-const li = document.createElement("li");
-ul.append(li);
+// const ul = document.createElement("ul");
+// const li = document.createElement("li");
+// const li2 = document.createElement("li");
+// document.body.append(ul);
+// ul.append(li);
+// ul.appendChild(li2);
+// li.innerText = "item";
+// li2.innerText = "item2";
+// const liText = li.innerText;
+// const li2Text = li2.innerText;
 
-console.log(ul.childNodes); // NodeList [ li ]
+// console.log(liText); // item
+// console.log(li2Text); // item2
+
+{
+	// create a new paragraph and append it to the end of body
+	const body = document.createElement("body");
+	const p = document.createElement("p");
+	document.body.appendChild(p);
+	p.innerText = "I am an appended paragraph!";
+	const appendedP = p.innerText;
+	// now append it to a newly created div element
+	const div = document.createElement("div");
+	document.body.appendChild(div);
+	div.appendChild(p);
+}
