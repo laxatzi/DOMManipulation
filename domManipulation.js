ffd1506c27238cc58fd293cf731e13812494a101
laxatzi <laxatzi@yahoo.com>
2020-12-18 13:59:19 +0200

    ADD: create elements and add them to the doc using append and appendChild methods

--- domManipulation.js
+++ domManipulation.js
@@ -1,28 +1,9 @@
 "use strict";
 
-// const ul = document.createElement("ul");
-// const li = document.createElement("li");
-// const li2 = document.createElement("li");
-// document.body.append(ul);
-// ul.append(li);
-// ul.appendChild(li2);
-// li.innerText = "item";
-// li2.innerText = "item2";
-// const liText = li.innerText;
-// const li2Text = li2.innerText;
+const messageDiv = document.createElement("div");
+document.body.append(messageDiv);
 
-// console.log(liText); // item
-// console.log(li2Text); // item2
+const message = document.createElement("p");
+const secMessage = document.createElement("p");
 
-{
-	// create a new paragraph and append it to the end of body
-	const body = document.createElement("body");
-	const p = document.createElement("p");
-	document.body.appendChild(p);
-	p.innerText = "I am an appended paragraph!";
-	const appendedP = p.innerText;
-	// now append it to a newly created div element
-	const div = document.createElement("div");
-	document.body.appendChild(div);
-	div.appendChild(p);
-}
+messageDiv.append(message, secMessage);
